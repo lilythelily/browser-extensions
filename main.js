f8bde2473778a905c0ef8f0f0d8e9d628d81b451
lilythelily <lilythelily@gmail.com>
2025-06-09 17:37:01 +0900

    updated

--- main.js
+++ main.js
@@ -4,7 +4,6 @@ const darkModeBtn = document.querySelector(".darkmode-btn");
 const darkNav = document.querySelector("nav");
 const lightNav = document.querySelector(".light-nav");
 const darkThreeBtns = document.querySelector(".three-btns");
-const lightThreeBtns = document.querySelector(".light-three-btns");
 const lightBg = document.querySelector(".light-bg");
 const h1 = document.querySelector("h1");
 const allBtn = document.querySelector("#all");
@@ -93,16 +92,45 @@ async function fetchJson() {
           btn.style.borderColor = "hsla(226, 11%, 37%, 0.5)";
           btn.style.color = "hsl(226, 11%, 37%)";
         });
+
         darkNav.style.display = "none";
         lightNav.style.display = "flex";
         lightBg.style.display = "block";
         h1.style.color = "hsl(227, 75%, 14%)";
+
         allBtn.classList.remove("active-btn");
         allBtn.classList.add("light-active-btn");
         activeBtn.classList.remove("inactive-btn");
         activeBtn.classList.add("light-inactive-btn");
         inactiveBtn.classList.remove("inactive-btn");
         inactiveBtn.classList.add("light-inactive-btn");
+
+        allBtn.addEventListener("click", (e) => {
+          allBtn.classList.remove("light-inactive-btn");
+          allBtn.classList.add("light-active-btn");
+          activeBtn.classList.remove("light-active-btn");
+          activeBtn.classList.add("light-inactive-btn");
+          inactiveBtn.classList.remove("light-active-btn");
+          inactiveBtn.classList.add("light-inactive-btn");
+        });
+
+        activeBtn.addEventListener("click", (e) => {
+          activeBtn.classList.remove("light-inactive-btn");
+          activeBtn.classList.add("light-active-btn");
+          allBtn.classList.remove("light-active-btn");
+          allBtn.classList.add("light-inactive-btn");
+          inactiveBtn.classList.remove("light-active-btn");
+          inactiveBtn.classList.add("light-inactive-btn");
+        });
+
+        inactiveBtn.addEventListener("click", (e) => {
+          inactiveBtn.classList.remove("light-inactive-btn");
+          inactiveBtn.classList.add("light-active-btn");
+          activeBtn.classList.remove("light-active-btn");
+          activeBtn.classList.add("light-inactive-btn");
+          allBtn.classList.remove("light-active-btn");
+          allBtn.classList.add("light-inactive-btn");
+        });
       });
 
       darkModeBtn.addEventListener("click", (e) => {
@@ -134,40 +162,92 @@ async function fetchJson() {
 
       toggleBtn.forEach((btn) => {
         btn.addEventListener("click", (e) => {
-          btn.classList.toggle("toggle-btn");
-          btn.classList.toggle("inactive-toggle-btn");
-          let activeData = e.target.parentNode.parentNode;
-          activeData.setAttribute("data-inactive", "data-inactive");
-          card.setAttribute("data-inactive", "data-inactive");
-          // card.removeAttribute("data-active");
+          const card = e.target.closest(".card");
+          if (card.dataset.active) {
+            btn.classList.remove("toggle-btn");
+            btn.classList.add("inactive-toggle-btn");
+            card.dataset.inactive = "data-inactive";
+            delete card.dataset.active;
+          } else {
+            btn.classList.remove("inactive-toggle-btn");
+            btn.classList.add("toggle-btn");
+            card.dataset.active = "data-active";
+            delete card.dataset.inactive;
+          }
         });
       });
 
       inactiveToggleBtn.forEach((btn) => {
         btn.addEventListener("click", (e) => {
-          btn.classList.toggle("inactive-toggle-btn");
-          btn.classList.toggle("toggle-btn");
-          let inactiveData = e.target.parentNode.parentNode;
-          inactiveData.setAttribute("data-active", "data-active");
-          card.setAttribute("data-active", "data-active");
-          // card.removeAttribute("data-inactive");
+          const card = e.target.closest(".card");
+          if (card.dataset.inactive) {
+            btn.classList.remove("inactive-toggle-btn");
+            btn.classList.add("toggle-btn");
+            card.dataset.active = "data-active";
+            delete card.dataset.inactive;
+          } else {
+            btn.classList.remove("toggle-btn");
+            btn.classList.add("inactive-toggle-btn");
+            card.dataset.inactive = "data-inactive";
+            delete card.dataset.active;
+          }
         });
       });
 
       // all/active/inactive lists
 
-      allBtn.addEventListener("click", (e) => {});
+      allBtn.addEventListener("click", (e) => {
+        allBtn.classList.remove("inactive-btn");
+        allBtn.classList.add("active-btn");
+        activeBtn.classList.remove("active-btn");
+        activeBtn.classList.add("inactive-btn");
+        inactiveBtn.classList.remove("active-btn");
+        inactiveBtn.classList.add("inactive-btn");
+        const cards = document.querySelectorAll(".card");
+        cards.forEach((card) => {
+          card.classList.remove("hidden");
+        });
+      });
 
       activeBtn.addEventListener("click", (e) => {
-        if (card.hasAttribute("data-inactive")) {
-          card.classList.add("hidden");
-        }
+        activeBtn.classList.remove("inactive-btn");
+        activeBtn.classList.add("active-btn");
+        allBtn.classList.remove("active-btn");
+        allBtn.classList.add("inactive-btn");
+        inactiveBtn.classList.remove("active-btn");
+        inactiveBtn.classList.add("inactive-btn");
+        console.log(card);
+        const cards = document.querySelectorAll(".card");
+        cards.forEach((card) => {
+          if (card.dataset.active) {
+            card.classList.remove("hidden");
+          } else {
+            card.classList.add("hidden");
+          }
+        });
       });
 
       inactiveBtn.addEventListener("click", (e) => {
-        if (card.hasAttribute("data-active")) {
-          card.classList.add("hidden");
-        }
+        inactiveBtn.classList.remove("inactive-btn");
+        inactiveBtn.classList.add("active-btn");
+        activeBtn.classList.remove("active-btn");
+        activeBtn.classList.add("inactive-btn");
+        allBtn.classList.remove("active-btn");
+        allBtn.classList.add("inactive-btn");
+        const cards = document.querySelectorAll(".card");
+        cards.forEach((card) => {
+          if (card.dataset.inactive) {
+            card.classList.remove("hidden");
+          } else {
+            card.classList.add("hidden");
+          }
+        });
+      });
+      newRemoveBtn.forEach((btn) => {
+        btn.addEventListener("click", (e) => {
+          let removedCard = e.target.parentNode.parentNode;
+          removedCard.classList.add("hidden");
+        });
       });
     });
   } catch (err) {
@@ -176,14 +256,3 @@ async function fetchJson() {
 }
 
 fetchJson();
-
-function darkMode() {
-  darkNav.style.display = "flex";
-  lightNav.style.display = "none";
-  darkThreeBtns.style.display = "flex";
-  lightBg.classList.toggle("hidden");
-  h1.style.color = "hsl(200, 60%, 99%)";
-  allBtn.classList.toggle("active-btn");
-  activeBtn.classList.toggle("inactive-btn");
-  inactiveBtn.classList.toggle("inactive-btn");
-}
